401c895482369741351c2e7d11a4450a93d70261
davepodgorski <davepodgorski@gmail.com>
2019-01-06 17:32:28 -0500

    practice

---
+++ todo.js
@@ -0,0 +1,15 @@
+var tasks, text, tLen;
+
+tasks = ["laundry", "shovel driveway", "grocery shopping", "check email", "daydream", "phone parents", "fill prescription", "exercise", "take out recycling", "check the weather"];
+tLen = tasks.length;
+
+
+text = "foo"
+
+console.log("Sunday To Do List:");
+
+for (i = 0; i < tLen; i++) {
+  console.log (`${i+1}): ${tasks[i]}`);
+}
+text += "foo"
+  
